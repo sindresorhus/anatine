2865ae937b71def7204bd14a64f1190f21423059
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-14 20:16:11 +0700

    fix some lint issues

--- browser.js
+++ browser.js
@@ -16,6 +16,8 @@ c);"function"===typeof define&&define.amd&&define(function(){return c})})(window
 /* mousetrap-global-bind.js */
 (function(a){var c={},d=a.prototype.stopCallback;a.prototype.stopCallback=function(e,b,a,f){return this.paused?!0:c[a]||c[f]?!1:d.call(this,e,b,a)};a.prototype.bindGlobal=function(a,b,d){this.bind(a,b,d);if(a instanceof Array)for(b=0;b<a.length;b++)c[a[b]]=!0;else c[a]=!0};a.init()})(Mousetrap);
 
+/* eslint-enable */
+
 /*
 
 This is copied from:
@@ -70,13 +72,12 @@ const scrollToTweet = event => { // eslint-disable-line
 	window.scrollTo(0, scrollTarget);
 };
 
-//----------------------------------------------//
+// ---------------------------------------------- //
 
 /* globals Mousetrap */
 const electron = require('electron');
 const ipc = electron.ipcRenderer;
 const remote = electron.remote;
-const path = remote.require('path');
 const osxAppearance = require('electron-osx-appearance');
 const storage = remote.require('./storage');
 const $ = document.querySelector.bind(document);
