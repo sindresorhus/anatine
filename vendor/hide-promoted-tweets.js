db3476b0bf5b52a46cf29593932f212851d28c5e
Paul Molluzzo <paulmolluzzo@users.noreply.github.com>
2016-06-14 16:08:26 -0400

    Move hidePromotedTweets to new file (#91)

---
+++ vendor/hide-promoted-tweets.js
@@ -0,0 +1,23 @@
+(() => {
+	'use strict';
+
+	window.hidePromotedTweets = waitFor => {
+		const seekAndDestroy = () => waitFor('.vjrx_CgX').then(el => {
+			el.closest('div[class*="_222QxFjc"][role="row"]').style.display = 'none';
+		});
+
+		waitFor('._1nQuzuNK._3tixQkQf > ._3tixQkQf').then(tweetContainer => {
+			// hide any immediately seen ads
+			seekAndDestroy();
+
+			// watch tweetContainer to hide new ads that get added
+			new MutationObserver(() => {
+				seekAndDestroy();
+			}).observe(tweetContainer, {attributes: true, childList: true});
+		});
+	};
+
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = window.hidePromotedTweets;
+	}
+})();
